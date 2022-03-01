bcf5f5b2a033658b3c662577d08c19260f3c7243
bariknishan <barik6286@gmail.com>
2022-03-02 02:20:07 +0600

    something aded

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-// console.log("hello")
+////////// serach area ///////////
 
 
-
-const searchPhone = () => {
+const searchPhone = (phones) => {
   const searchField = document.getElementById("search-Field");
   const searchText = searchField.value;
-  console.log(searchText);
+  // console.log(searchText);
   searchField.value = "";
  
-  if (searchText ==""  ) {
+  if (searchText == "" )  {
     // alert("Search something")
     document.getElementById("error-field").style.display = "block";
+
     document.getElementById("search-result").style.display="none";
   }
 
   else {
     document.getElementById("error-field").style.display = "none";
-
     const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
     // console.log(url);
     fetch(url)
       .then(res => res.json())
       .then(phones => displaySearchResults(phones.data.slice(0, 20)));
+      
+      
   }
 };
+
   const displaySearchResults = phones => {
-   
   const searchPhoneResult = document.getElementById("search-result");
   searchPhoneResult.textContent = "";
   phones.forEach(phone => {
-    // console.log(phone);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `  
@@ -38,7 +38,7 @@ const searchPhone = () => {
           <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <h6 class="card-brand">Brand:${phone.brand}</h6>
-            <button  onclick= "loadPhoneDetails('${phone.slug}')"  type="button" class="btn btn-primary w-75 mx-auto">Details</button>
+            <button  onclick= "loadPhoneDetails('${phone.slug}') "  type="button" class="btn btn-primary w-75 mx-auto">Details</button>
            
           </div>
         </div>
@@ -46,11 +46,12 @@ const searchPhone = () => {
        `;
 
     searchPhoneResult.appendChild(div);
+    
   });
 }
 
 const loadPhoneDetails = phoneId => {
-  // console.log(phoneId);
+  
   const url = `https://openapi.programming-hero.com/api/phone/${phoneId} `
   fetch(url)
     .then(res => res.json())
@@ -61,32 +62,32 @@ const loadPhoneDetails = phoneId => {
 ///////////phone Details Area////////////////
 
 const displayPhoneDetails = phone => {
-  //  console.log(phone)
+ 
   const phoneDetails = document.getElementById("phone-details");
   phoneDetails.textContent = "";
   const div = document.createElement("div");
   div.classList.add("card")
   div.innerHTML = `<img   src= "${phone.image}" class="card-img-top" alt="...">
-   <div class="card-body">
+   <div class="card-body ">
      <h5 class="card-title  fw-bold">${phone.name}</h5>
      <h6 class="card-title fw-bold">Brand:${phone.brand}</h6>
      <h6 class="card-title fw-bold ">Model ID:${phone.slug}</h6>
      <h4 class="card-title fw-bold " > Main Features:</h4>
      <p class="card-text  ">Storage:${phone.mainFeatures.storage}</p>
      <p class="card-text ">Sensor: ${phone.mainFeatures.sensors}</p>
-     <h4 class="card-title fw-bold " > Other Features:</h4>
+     <h4 class="card-title fw-bold"> Other Features:</h4>
      <p class="card-text ">WLAN: ${phone.others?.WLAN}</p>
      <p class="card-text ">Blootooth: ${phone.others?.Bluetooth}</p>
      <p class="card-text ">GPS: ${phone.others?.GPS}</p>
      <p class="card-text ">NFC: ${phone.others?.NFC}</p>
      <p class="card-text ">RADIO: ${phone.others?.Radio}</p>
-     if(${phone.others==""}){
-     console.log(" result not found")
-     }
      <p class="card-text ">USB: ${phone.others?.USB}</p>
      <p class="card-text ">Relase Date:${phone.releaseDate}</p>
    </div>  `;
 
   phoneDetails.appendChild(div);
-
+  
+  
+  
 }
+
